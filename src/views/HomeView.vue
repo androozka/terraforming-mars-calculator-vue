fd26dddd05dc6414c46b59bf558d2e9b32f47068
androozka <androozka@gmail.com>
2024-07-24 19:01:31 -0700

    updated router and base pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home Page</h1>
   </main>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
